f851d82428a3789efd10dc83431e645ad393cd46
Xestra1 <33702536+Xestra1@users.noreply.github.com>
2019-06-07 09:29:28 -0400

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,12 @@
+var bill = document.getElementById("bill").value;
+var tipPercent = document.getElementById("tipPercent").value;
+var numOfPeople = document.getElementById("numOfPeople").value;
+
 document.getElementById("totalTip").innerHTML = 0;
 document.getElementById("total").innerHTML = 0;
 
 
 function calculator() {
-    var bill = document.getElementById("bill").value;
-    var tipPercent = document.getElementById("tipPercent").value;
-    var numOfPeople = document.getElementById("numOfPeople").value;
-
     // founding the tip
     tipPercent /= 100;
     tipPercent *= bill;
@@ -17,5 +17,4 @@ function calculator() {
     var total = Number(tipPercent) + Number(bill);
     var newTotal = Number(total) / Number(numOfPeople);
     document.getElementById("total").innerHTML = newTotal;
-
 }
