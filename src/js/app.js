9536b4eab1662b2b7d58b74dac28ce77e667b337
Xestra1 <33702536+Xestra1@users.noreply.github.com>
2019-06-13 19:27:26 -0400

    gave it a little update

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,22 @@
 var bill = document.getElementById("bill").value;
 var tipPercent = document.getElementById("tipPercent").value;
 var numOfPeople = document.getElementById("numOfPeople").value;
+var times = 0;
 
 document.getElementById("totalTip").innerHTML = 0;
 document.getElementById("total").innerHTML = 0;
 
+function run() {
+    if (times == 0) {
+
+        calculator();
+        console.log("ter");
+    } else {
+        calculator();
+        calculator();
+        console.log("tetrer");
+    }
+}
 
 function calculator() {
     // founding the tip
@@ -17,4 +29,9 @@ function calculator() {
     var total = Number(tipPercent) + Number(bill);
     var newTotal = Number(total) / Number(numOfPeople);
     document.getElementById("total").innerHTML = newTotal;
+    bill = document.getElementById("bill").value;
+    tipPercent = document.getElementById("tipPercent").value;
+    numOfPeople = document.getElementById("numOfPeople").value;
+
+    times++;
 }
